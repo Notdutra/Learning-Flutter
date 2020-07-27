6b75075630c92cd1451086ecd84af8a8bba1e93c
NotDutra <arthurdutra@live.ca>
2020-07-27 12:54:13 -0300

    update

--- Tutorials/AppBrewery/i_am_rich/lib/main.dart
+++ Tutorials/AppBrewery/i_am_rich/lib/main.dart
@@ -5,8 +5,16 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.red[900],
         appBar: AppBar(
           title: Text("I Am Rich"),
+          backgroundColor: Colors.red[900],
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://assets.entrepreneur.com/content/3x2/2000/20160305000536-diamond.jpeg')
+          ),
         ),
       ),
     ),
